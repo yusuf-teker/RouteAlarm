6c1c06ee736603483f3d9ce1395eac0a30755248
Yusuf Teker <y.teker.17@protonmail.com>
2025-05-05 11:43:07 +0300

    * Home Screen için aktif alarm göstergesi eklendi.

--- composeApp/src/androidMain/kotlin/org/yusufteker/routealarm/Previews.kt
+++ composeApp/src/androidMain/kotlin/org/yusufteker/routealarm/Previews.kt
@@ -1,8 +1,18 @@
 package org.yusufteker.routealarm
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.yusufteker.routealarm.core.presentation.AppColors
 import org.yusufteker.routealarm.feature.alarm.presentation.home.HomeScreen
+import org.yusufteker.routealarm.feature.alarm.presentation.home.components.StopProgressBar
 import org.yusufteker.routealarm.feature.onboarding.presentation.welcome.WelcomeScreen
 
 @Preview
@@ -16,6 +26,32 @@ fun WelcomeScreenPreview() {
 @Composable
 fun HomeScreenPreview() {
     HomeScreen(onAddAlarm = {}, onAlarmClick = {}, onSettingsClick = {})
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun StopProgressBarPreview() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF121212)) // Koyu arka plan
+                .padding(16.dp)
+        ) {
+
+            StopProgressBar(
+                title = "Başlık",
+                passedStops = 1,
+                stopNames = listOf("Start", "Dest1", "Dest2"),
+                modifier = Modifier.background(
+                    AppColors.cardBackground
+                )
+            )
+
+
+        }
+    }
 }
 
 
--- composeApp/src/commonMain/kotlin/org/yusufteker/routealarm/feature/alarm/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/yusufteker/routealarm/feature/alarm/presentation/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package org.yusufteker.routealarm.feature.alarm.presentation.home
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
@@ -7,7 +8,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import org.yusufteker.routealarm.feature.alarm.presentation.home.components.StopProgressBar
 
 @Composable
 fun HomeScreen(
@@ -15,7 +18,7 @@ fun HomeScreen(
 ) {
     Scaffold { paddingValues ->
         Column(
-            modifier = Modifier.fillMaxSize().padding(paddingValues).padding(16.dp),
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -32,6 +35,26 @@ fun HomeScreen(
             Button(onClick = onSettingsClick) {
                 Text("Settings")
             }
+            ActiveAlarm()
         }
+    }
+}
+
+
+@Composable
+fun ActiveAlarm(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF121212)) // Koyu arka plan
+            .padding(vertical = 16.dp)
+    ) {
+
+        StopProgressBar(
+            title = "Başlık",
+            passedStops = 1,
+            stopNames = listOf("Start", "Destination1", "Destination2","Destination3"),
+            )
+
     }
 }
