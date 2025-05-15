ae3286c6e9f0039cf31fd5c9b4f0cb9df2815001
Yusuf Teker <y.teker.17@protonmail.com>
2025-05-15 09:57:04 +0300

    * İos Map implementasyonu kaldırıldı * İos PlaceSuggestionService mini fix * İhtiyaç dışı kütüphaneler kaldırıldı

--- composeApp/src/iosMain/kotlin/org/yusufteker/routealarm/feature/location/presentation/PlatformMap.ios.kt
+++ composeApp/src/iosMain/kotlin/org/yusufteker/routealarm/feature/location/presentation/PlatformMap.ios.kt
@@ -1,15 +1,9 @@
 package org.yusufteker.routealarm.feature.location.presentation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.UIKitView
-import kotlinx.cinterop.ExperimentalForeignApi
 import org.yusufteker.routealarm.feature.location.domain.Location
-import platform.CoreLocation.CLLocationCoordinate2DMake
-import platform.MapKit.MKMapView
 
-@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun PlatformMap(
     modifier: Modifier,
@@ -21,23 +15,4 @@ actual fun PlatformMap(
 
 ) {
 
-    val clLocation = remember(currentLocation) {
-        CLLocationCoordinate2DMake(
-            currentLocation.lat,
-            currentLocation.lng
-        )
-    }
-    UIKitView(
-        modifier = Modifier,
-        factory = {
-            MKMapView().apply {
-                setZoomEnabled(true)
-                setScrollEnabled(true)
-                // customize map view further
-            }
-        },
-        update = { mapView ->
-            mapView.setCenterCoordinate(clLocation, animated = true)
-        }
-    )
 }
